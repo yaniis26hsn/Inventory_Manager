effb6a9ef623c511c3b204c1442bf0c60b7747e4
yaniishsn <hassainiyanis2006@gmail.com>
2026-07-29 09:33:26 +0100

    switching to express

--- BACKEND/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Body, Controller, Get , Param, Post, Query, Req, Res } from '@nestjs/common';
-import { AppService } from './app.service';
-
-@Controller()
-export class AppController {
-  constructor(private readonly aappService: AppService) {}
-
-
-}
--- BACKEND/src/common/enums/role.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum Role {
- ADMIN = 'admin' ,
- Worker = 'worker' ,
- Provider = 'provider'
-}
